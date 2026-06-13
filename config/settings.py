eb1a85a54d276c12d2b6e0f565f314529cf3067f
jfalla-bit <fallagarciajulianaugusto@gmail.com>
2026-06-13 14:57:33 -0500

    Actualizar configuración para Railway

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config(
+    "SECRET_KEY",
+    default="django-insecure-temporal-key"
+)
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
