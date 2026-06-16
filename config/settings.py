433bfe8520da5ee8da3485bfa3d506ad549ea142
jfalla-bit <fallagarciajulianaugusto@gmail.com>
2026-06-16 18:50:25 -0500

    fix allowed hosts for railway

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,7 @@ WOMPI_API_URL       = 'https://sandbox.wompi.co/v1'
 
 # ── Producción ──────────────────────────────────────────────────────────────
 import os
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ["*"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
